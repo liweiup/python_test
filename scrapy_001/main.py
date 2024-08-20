b678a7f1c22017651377d680196a05b2d4b34ded
liwei <li.play@qq.com>
2024-08-20 16:19:06 +0800

    xx

--- scrapy_001/main.py
+++ scrapy_001/main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # print([1,2] * [1,2])
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
